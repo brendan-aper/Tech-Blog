2b40a9e536e352139c09d9c38cfaf030ad38d062
Brendan Aper <brendanaper@gmail.com>
2023-08-10 12:19:11 -0700

    added api routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require("./controllers")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Listeing on PORT: ${PORT}`));
